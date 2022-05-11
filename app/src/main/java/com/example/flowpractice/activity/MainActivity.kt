1a574f6e38ab468f6e16dadfea2fe3139aaa2ceb
ping <xiejianping@leomaster.com>
2022-05-11 11:12:48 +0800

    小修改

--- app/src/main/java/com/example/flowpractice/activity/MainActivity.kt
+++ app/src/main/java/com/example/flowpractice/activity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.flowpractice.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blankj.utilcode.util.BarUtils
 import com.example.flowpractice.R
 import com.example.flowpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val mBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -13,6 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
-
+        BarUtils.setStatusBarLightMode(this,true)
     }
 }
